fbdd41b55a59eaa82c393936c960db605cbaeda9
Administrator <Administrator@192.168.0.6>
2009-04-04 14:27:27 +0800

    error 消除

--- JavaSource/jp/co/sisc/ams/test/AdminRlDaoImplTest.java
+++ JavaSource/jp/co/sisc/ams/test/AdminRlDaoImplTest.java
@@ -1,15 +1,10 @@
 package jp.co.sisc.ams.test;
 
-import jp.co.sisc.ams.dao.mast1600.IPartnerDao;
-import jp.co.sisc.ams.domain.mast1600.Partner;
-import jp.co.sisc.ams.service.mast1600.IPartnerService;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 
@@ -20,10 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 @ContextConfiguration("/spring-config.xml")
 public class AdminRlDaoImplTest {
 
-	@Autowired
-	private IPartnerDao dao;
-	@Autowired
-	private IPartnerService service;
+	//@Autowired
+	//private IPartnerDao dao;
+	//@Autowired
+	//private IPartnerService service;
 //
 //	@Test
 //	public void testSelectListT() {
@@ -37,14 +32,14 @@ public class AdminRlDaoImplTest {
 //	}
 	@Test
 	public void testAop() {
-		Partner p = new Partner();
-		p.setPar_cd("1");
-		p.setPar_nm("2222");
-		p.setPms_i_usr("kk");
-		p.setPms_i_class("ss");
-		//dao.updateById(p);
-		
-		service.updateById(p);
+//		Partner p = new Partner();
+//		p.setPar_cd("1");
+//		p.setPar_nm("2222");
+//		p.setPms_i_usr("kk");
+//		p.setPms_i_class("ss");
+//		//dao.updateById(p);
+//		
+//		service.updateById(p);
 	}
 	
 	public void main(String[] args) {
